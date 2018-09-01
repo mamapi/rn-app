18843cba833f3bb35d80abb6d6b8cf348bbfb47a
Gregory G <gorczynski.grzegorz@gmail.com>
2018-09-01 10:55:47 +0200

    [#1.3] styling applied

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Dimensions } from 'react-native';
+import { Constants } from 'expo'
 
 export default class App extends React.Component {
 
   state = {
-    items: new Array(100).fill(0).map((a, i) => i).map(i => ({
-      key: i,
-      title: `Note numer ${i}`,
+    items: new Array(20).fill(0).map((a, i) => i).map(i => ({
+      key: `${i}`,
+      title: `Note no ${i}`,
       content: `Lorem ipsum dolor sit amet.`,
     })),
   };
@@ -21,8 +22,8 @@ export default class App extends React.Component {
 
   renderItem = ({ item }) => (
     <View style={styles.item}>
-      <Text style={styles.title}>{item.title}</Text>
-      <Text style={styles.content}>{item.content}</Text>
+      <Text style={styles.itemTitle}>{item.title}</Text>
+      <Text style={styles.itemContent}>{item.content}</Text>
     </View>
   );
 
@@ -31,8 +32,21 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    padding: 0,
+    margin: 0,
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    alignSelf: "stretch",
   },
+  item: {
+    paddingHorizontal: 10,
+  },
+  itemTitle: {
+    fontWeight: 'bold',
+    marginVertical: 5,
+  },
+  itemContent: {
+    marginBottom: 10,
+  }
 });
