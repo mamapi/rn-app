15f14c96b24e73a0e5a2ef07e81834a03ed818dc
Gregory G <gorczynski.grzegorz@gmail.com>
2018-09-01 12:23:03 +0200

    [#2.1] form to adding new note added

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View, FlatList, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Button, TextInput } from 'react-native';
 import { Constants } from 'expo'
+import { Ionicons } from '@expo/vector-icons'
 
+const STATUS_DONE = 'Done'
+const STATUS_NEW = 'New'
 export default class App extends React.Component {
 
   state = {
-    items: new Array(20).fill(0).map((a, i) => i).map(i => ({
-      key: `${i}`,
-      title: `Note no ${i}`,
-      content: `Lorem ipsum dolor sit amet.`,
-    })),
-  };
+    newTitle: '',
+    newContent: '',
+    notes: [],
+  }
 
   render() {
     return (
       <View style={styles.container}>
-        <FlatList data={this.state.items} renderItem={this.renderItem} />
+
+        <TextInput
+          placeholder="Enter note title"
+          returnKeyType="done"
+          value={this.state.newTitle}
+          onChangeText={this.titleTextChanged}
+        />
+        <TextInput
+          placeholder="Enter note content"
+          returnKeyType="done"
+          value={this.state.newContent}
+          onChangeText={this.contentTextChanged}
+        />
+        <Button style={styles.itemStatusButton}
+          onPress={() => this.addSubmit()}
+          title="Add"
+        />
+
+        <FlatList data={this.state.notes} renderItem={this.renderItem} />
       </View>
     );
   }
@@ -24,10 +43,41 @@ export default class App extends React.Component {
     <View style={styles.item}>
       <Text style={styles.itemTitle}>{item.title}</Text>
       <Text style={styles.itemContent}>{item.content}</Text>
-    </View>
-  );
 
+      <StatusIcon {...item} />
+
+    </View>
+  )
+
+  titleTextChanged = text =>
+    this.setState(state => ({
+      newTitle: text,
+    }))
+
+  contentTextChanged = text =>
+    this.setState(state => ({
+      newContent: text,
+    }))
+
+  addSubmit() {
+    this.setState(state => ({
+      notes: state.notes.concat({
+        title: state.newTitle,
+        content: state.newContent,
+        status: STATUS_NEW,
+      }),
+      newTitle: '',
+      newContent: '',
+    }))
+  }
 }
+
+StatusIcon = ({ status }) => (
+  <Ionicons
+    name={status == "Done" ? "md-checkmark-circle" : "md-checkmark-circle-outline"}
+    size={32}
+    color={status == "Done" ? "green" : "gray"} />
+)
 
 const styles = StyleSheet.create({
   container: {
@@ -48,5 +98,8 @@ const styles = StyleSheet.create({
   },
   itemContent: {
     marginBottom: 10,
+  },
+  itemStatusButton: {
+    justifyContent: 'center'
   }
 });
