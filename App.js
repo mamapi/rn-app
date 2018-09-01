a065b5947352338c1ca021e9114d9cb735d56e66
Gregory G <gorczynski.grzegorz@gmail.com>
2018-09-01 10:05:06 +0200

    [#1.2] list of notes added

--- App.js
+++ App.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default class App extends React.Component {
+
+  state = {
+    items: new Array(100).fill(0).map((a, i) => i).map(i => ({
+      key: i,
+      title: `Note numer ${i}`,
+      content: `Lorem ipsum dolor sit amet.`,
+    })),
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <FlatList data={this.state.items} renderItem={this.renderItem} />
       </View>
     );
   }
+
+  renderItem = ({ item }) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{item.title}</Text>
+      <Text style={styles.content}>{item.content}</Text>
+    </View>
+  );
+
 }
 
 const styles = StyleSheet.create({
